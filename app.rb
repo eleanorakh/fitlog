2ef87423e640293bc6bf8de06083f9254a647d02
Eleanor Kiefel Haggerty <eleanorakh@gmail.com>
2013-05-28 20:20:50 +1000

    Add Workout show and index pages

--- app.rb
+++ app.rb
@@ -19,3 +19,14 @@ end
 get '/users/:id' do
   User.get(params[:id]).name || 'unknown'
 end
+
+get '/users/:user_id/workouts' do
+  user = User.get(params[:user_id])
+  user.workouts.map do |workout|
+    workout.exercise_type
+  end.join ', '
+end
+
+get '/users/:user_id/workouts/:id' do
+  Workout.get(params[:id]).exercise_type
+end
