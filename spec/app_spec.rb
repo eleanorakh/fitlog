247a477dc5a9e7d83f7f889cf89348598a8b97a2
Eleanor Kiefel Haggerty <eleanorakh@gmail.com>
2013-07-06 12:17:12 +1000

    Use capybara for testing

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,26 +3,30 @@ ENV['RACK_ENV'] = 'test'
 require File.expand_path('./app.rb')
 
 require 'rspec'
-require 'rack/test'
+require 'capybara'
+require 'capybara/dsl'
 require 'database_cleaner'
 
 describe 'fitlog' do
   before { DatabaseCleaner.start }
   after { DatabaseCleaner.clean }
 
-  include Rack::Test::Methods
+  include Capybara::DSL
 
   def app
     Sinatra::Application
   end
 
+  before do
+    Capybara.app = app
+  end
+
   describe 'users index' do
     context 'with no users' do
       it 'displays a message' do
-        get '/users'
-        expect(last_response).to be_ok
+        visit '/users'
         expect(names_from_html).to be_empty
-        empty_text = Nokogiri::HTML(last_response.body).css('p').first.text.strip
+        empty_text = Nokogiri::HTML(page.html).css('p').first.text.strip
         expect(empty_text).to eq 'No users.'
       end
     end
@@ -31,8 +35,7 @@ describe 'fitlog' do
       it 'displays the user names' do
         User.new(:name => 'Ron').save
         User.new(:name => 'Harry').save
-        get '/users'
-        expect(last_response).to be_ok
+        visit '/users'
         expect(names_from_html).to match_array ['Ron', 'Harry']
       end
     end
@@ -42,15 +45,14 @@ describe 'fitlog' do
     it 'displays the user' do
       ron = User.new(:name => 'Ron')
       ron.save
-      get "/users/#{ron.id}"
-      expect(last_response).to be_ok
-      expect(last_response.body).to match /Ron/
+      visit "/users/#{ron.id}"
+      expect(page).to have_content 'Ron'
     end
   end
 end
 
 def names_from_html
-  Nokogiri::HTML(last_response.body).css('html body ul li').map do |list_item|
+  Nokogiri::HTML(page.html).css('html body ul li').map do |list_item|
     list_item.text.strip
   end
 end
